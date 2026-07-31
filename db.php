0a3c26581346d8e84204015dbafa80c0cd6608bc
saigayu16 <saigayu1605@gmail.com>
2026-07-31 14:17:35 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,4 @@
 <?php
-// Enable error reporting for debugging
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -7,14 +6,11 @@ error_reporting(E_ALL);
 $dsn = "pgsql:host=ep-damp-shadow-adjtp5ul-pooler.c-2.us-east-1.aws.neon.tech;port=5432;dbname=neondb;user=neondb_owner;password=npg_ywILa5u2sOtv;sslmode=require";
 
 try {
-    // Pastikan guna $pdo di sini supaya sepadan dengan auth.php
-    $pdo = new PDO($dsn, null, null, [
+    $conn = new PDO($dsn, null, null, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]);
-    
 } catch (PDOException $e) {
-    // Handle connection failure securely
     die("Sambungan Gagal: " . $e->getMessage());
 }
 ?>
