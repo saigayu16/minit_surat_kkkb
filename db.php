c125456a981014c0e7dab4e988fdf234969cc643
saigayu16 <saigayu1605@gmail.com>
2026-07-31 14:22:04 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,5 @@
 <?php
+// Enable error reporting for debugging
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -6,7 +7,7 @@ error_reporting(E_ALL);
 $dsn = "pgsql:host=ep-damp-shadow-adjtp5ul-pooler.c-2.us-east-1.aws.neon.tech;port=5432;dbname=neondb;user=neondb_owner;password=npg_ywILa5u2sOtv;sslmode=require";
 
 try {
-    $conn = new PDO($dsn, null, null, [
+    $pdo = new PDO($dsn, null, null, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]);
