5ff7fa79a10d38b23b0cfe2e43e691bc29eda5c3
saigayu16 <saigayu1605@gmail.com>
2026-08-04 08:49:42 +0800

    Update proses_makluman.php

--- proses_makluman.php
+++ proses_makluman.php
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $api_key = getenv('BREVO_API_KEY');
         
         $data = [
-            "sender" => ["email" => "saigayu1605@gmail.com", "name" => "Sistem Minit Digital"],
+            "sender" => ["email" => "kkkepalabatas@kkkb.edu.my", "name" => "Sistem Minit Digital"],
             "to" => [["email" => $email_staf]],
             "subject" => "Notifikasi: Dokumen Asal dan Minit Surat Baharu",
             "htmlContent" => "
