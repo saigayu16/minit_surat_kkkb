ad66147f42d784c71635e4c793b2d621a4f92def
saigayu16 <saigayu1605@gmail.com>
2026-08-11 08:33:00 +0800

    Update register.php

--- register.php
+++ register.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
     $username = trim($_POST['username'] ?? '');
     $email    = trim($_POST['email'] ?? '');
-    $password = password_hash($_POST['password'] ?? '', PASSWORD_DEFAULT); 
+    $password = $_POST['password'] ?? '';
     $role     = $_POST['role'] ?? '';
 
     try {
